f976bb417b7bb6e948b211bebd9d75ffb97fc4c5
HarshaKatta18 <harshakatta18@gmail.com>
2026-04-07 10:41:39 +0530

    updated main.tf with random bucket name

--- main.tf
+++ main.tf
@@ -2,10 +2,22 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_s3_bucket" "good_bucket" {
-  bucket = "my-secure-bucket"
+# 🔹 Add random suffix for unique bucket name
+resource "random_id" "suffix" {
+  byte_length = 4
 }
 
+# 🔹 S3 bucket with unique name
+resource "aws_s3_bucket" "good_bucket" {
+  bucket = "my-secure-bucket-${random_id.suffix.hex}"
+
+  tags = {
+    Environment = "dev"
+    Project     = "terraform-policy-lab"
+  }
+}
+
+# 🔹 Enable encryption
 resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
   bucket = aws_s3_bucket.good_bucket.id
 
